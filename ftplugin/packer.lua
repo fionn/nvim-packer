3ac74cc5fa418d763b3fe9bcc8629e32c351c95f
Fionn Fitzmaurice <fionn@github.com>
2026-06-21 23:22:51 +0800

    Add busy status and refactor
    Executing packer fmt can take a while, so set the busy status (and unset
    it afterwards). In order to make the setting and unsetting close to each
    other, move the formatting logic into a separate function.

--- ftplugin/packer.lua
+++ ftplugin/packer.lua
@@ -1,25 +1,41 @@
-if vim.fn.executable("packer") == 1 then
-    vim.api.nvim_create_autocmd("BufWritePre", {
-        group = vim.api.nvim_create_augroup("packer_format", {clear = true}),
-        desc = "Format Packer",
-        buffer = 0,
-        callback = function(ev)
-            local lines = vim.api.nvim_buf_get_lines(ev.buf, 0, -1, true)
-            local result = vim.system({"packer", "fmt", "-"}, {stdin = lines}):wait()
-            if result.code ~= 0 then
-                vim.notify("packer fmt: " .. result.stderr, vim.log.levels.ERROR)
-                return
-            end
+---@param buf integer
+local function format(buf)
+    if vim.fn.executable("packer") == 0 then
+        vim.notify("Skipping format as packer is missing", vim.log.levels.DEBUG)
+        return
+    end
 
-            local formatted = vim.split(result.stdout, "\n", {trimempty = false})
-            table.remove(formatted)  -- Pop the last line as it's empty.
+    local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, true)
+    local result = vim.system({"packer", "fmt", "-"}, {stdin = lines}):wait()
 
-            if not vim.deep_equal(lines, formatted) then
-                local view = vim.fn.winsaveview()
-                pcall(vim.cmd.undojoin)
-                vim.api.nvim_buf_set_lines(ev.buf, 0, -1, true, formatted)
-                vim.fn.winrestview(view)
-            end
-        end
-    })
+    if result.code ~= 0 then
+        vim.opt_local.busy = vim.opt.busy:get() - 1
+        vim.notify(("packer fmt exited with status %s: %s"):format(result.code,
+            result.stderr), vim.log.levels.ERROR)
+        return
+    end
+
+    local formatted = vim.split(result.stdout, "\n", {trimempty = false})
+    table.remove(formatted)  -- Pop the last line as it's empty.
+
+    if not vim.deep_equal(lines, formatted) then
+        local view = vim.fn.winsaveview()
+        pcall(vim.cmd.undojoin)
+        vim.api.nvim_buf_set_lines(buf, 0, -1, true, formatted)
+        vim.fn.winrestview(view)
+    end
 end
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    group = vim.api.nvim_create_augroup("packer_format", {clear = true}),
+    desc = "Format Packer",
+    buffer = 0,
+    callback = function(ev)
+        vim.opt_local.busy = vim.opt.busy:get() + 1
+        vim.api.nvim__redraw({buf = ev.buf, flush = true})
+        if not pcall(format, ev.buf) then
+            vim.notify("Error formatting buffer", vim.log.levels.ERROR)
+        end
+        vim.opt_local.busy = vim.opt.busy:get() - 1
+    end
+})
