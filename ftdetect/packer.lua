e3f1e9f885026851605303ed899bd4e3f965361f
Fionn Fitzmaurice <fionn@github.com>
2026-06-22 23:43:34 +0800

    Detect filetype with vim.filetype.add
    Give up on hashivim/vim-terraform, which frees us to do filetype
    detection and setting in a more standard way.

--- ftdetect/packer.lua
+++ ftdetect/packer.lua
@@ -1,12 +1,5 @@
--- We detect Packer files via an autocommand rather than using the recommended
--- vim.filetype.add mechanism because the latter fails to override the default
--- hcl filetype, possibly due to an interaction with hashivim/vim-terraform,
--- which we want to coexist with.
-
-vim.api.nvim_create_autocmd("BufEnter", {
-    desc = "Set Packer filetype",
-    pattern = {"*.pkr.hcl"},
-    callback = function()
-        vim.opt_local.filetype = "hcl.packer"
-    end
+vim.filetype.add({
+    pattern = {
+        [".+%.pkr%.hcl"] = "hcl.packer"
+    }
 })
